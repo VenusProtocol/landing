74729d270bac462faa8cd64aeafbd571c97ddc51
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-03-10 12:44:16 +0400

    Add basic setup of linter, husky, react-snap

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  return (
+    <main>
+      <header>header</header>
+      <section>main content</section>
+      <footer>footer</footer>
+    </main>
+  )
+}
+
+export default App
