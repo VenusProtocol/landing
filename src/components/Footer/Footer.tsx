9183558a812334df8eea544e5a914923ddc25101
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-03-15 14:11:20 +0400

    add footer components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,12 @@
 import React from "react"
 import cn from "classnames"
-import s from "./Footer.module.css"
 import Container from "../Container/Container"
+import NavigationLinks from "../NavigationLinks/NavigationLinks"
+import { ReactComponent as Logo } from "../Header/assets/logo.svg"
+import { ReactComponent as LogoMobile } from "./assets/logoSmall.svg"
+import Link from "../Link/Link"
+import SocialLinks from "./SocialLinks"
+import s from "./Footer.module.css"
 
 interface IFooterProps {
   className?: string
@@ -10,8 +15,24 @@ interface IFooterProps {
 const Footer: React.FC<IFooterProps> = ({ className }) => {
   return (
     <footer className={cn(s.root, className)}>
-      <Container>
-        <div>Footer</div>
+      <Container className={s.container}>
+        <LogoMobile className={s.logoMobile} />
+        <Logo className={s.logo} />
+        <NavigationLinks
+          classNames={{ root: s.footerNavLinksWrapper, link: s.footerLink }}
+        />
+        <SocialLinks className={s.socialLinksWrapperMobile} />
+        <Link className={s.btn} variant="buttonTransparent" href="TODO">
+          White paper
+        </Link>
+        <div className={s.copyWrapper}>
+          <p className={s.copy}>
+            © 2021 Developed by Swipe Wallet.
+            <br />
+            Licensed & open-source under the MIT License
+          </p>
+          <SocialLinks className={s.socialLinksWrapperDesktop} />
+        </div>
       </Container>
     </footer>
   )
