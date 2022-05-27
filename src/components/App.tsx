434f0795c009764c0283e13577df1fe224969aa5
Dmitriy Lisyanskiy <dmitriy.lisyanskiy@ankr.com>
2022-05-27 11:18:53 +0400

    add redirect for discord route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,18 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Header from "./Header/Header"
 import MainContent from "./MainContent/MainContent"
 import Footer from "./Footer/Footer"
 import s from "./App.module.css"
 
 function App() {
+  useEffect(() => {
+    if (window.location.pathname.startsWith("/discord")) {
+      window.location.replace(
+        "https://discord.com/servers/venus-protocol-912811548651708448"
+      )
+    }
+  }, [window.location.pathname])
+
   return (
     <main className={s.root}>
       <Header />
