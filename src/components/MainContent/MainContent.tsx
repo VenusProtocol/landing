a1294a5eb9abded41d94a754b13d383514525da2
Gleiser Oliveira <gleiser.dev@gmail.com>
2023-04-02 23:50:21 -0600

    feat: updated MainContent with new sections

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -4,6 +4,10 @@ import Intro from './Intro';
 import s from './MainContent.module.css';
 import Market from '../Market/Market';
 import Benefits from '../Benefits/Benefits';
+import Safety from '../Safety';
+import Protection from '../Protection';
+import VenusPrime from '../VenusPrime';
+import Governance from '../Governance';
 
 interface IMainContentProps {
   className?: string;
@@ -13,6 +17,10 @@ const MainContent: React.FC<IMainContentProps> = ({ className }) => (
   <section className={cn(s.root, className)}>
     <Intro />
     <Market />
+    <VenusPrime />
+    <Protection />
+    <Governance />
+    <Safety />
     <Benefits />
   </section>
 );
